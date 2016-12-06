c075697df0bd9d9fd2595d119c90047d24f2faaa
ericbsd <ericturgeon.bsd@gmail.com>
2016-12-05 21:17:11 -0400

    Adding user not setup for sudo

--- src/gbsudogui.py
+++ src/gbsudogui.py
@@ -42,6 +42,7 @@ import sys
 from time import sleep
 sys.path.append('/usr/home/ericbsd/gbsudo/')
 
+
 class askPassword():
     def delete(self, widget, event=None):
         Gtk.main_quit()
@@ -104,36 +105,12 @@ class askPassword():
 
     def setpassword(self, widget):
         self.window.hide()
-        trytimes += 1
+        self.trytimes += 1
 
-        if trytimes <= 3:
+        if self.trytimes <= 3:
             self.password = self.passwordentry.get_text()
             GLib.idle_add(self.sudopasswd, self.arg, self.password)
         else:
-            Gtk.main_quit()
-
-    def passwordAutentification(self, arg, psswrd):
-        #self.window.hide()
-        sudocomand = 'sudo ' + arg
-        sudochild = pexpect.spawn(sudocomand)
-        sudochild.expect(['Password:'])
-        sudochild.sendline(psswrd)
-        i = sudochild.expect(['is not in the sudoers file.', 'Password:', pexpect.EOF, pexpect.TIMEOUT])
-        if i == 0:
-            print 0
-            sudochild.kill(0)
-            Gtk.main_quit()
-        elif i == 1:
-            print 1
-            sudochild.kill(0)
-            self.passwordentry.set_text("")
-        elif i == 2:
-            print 2
-            sudochild.interact()
-            Gtk.main_quit()
-        elif i == 3:
-            self.window.hide()
-            sudochild.interact()
             Gtk.main_quit()
 
     def sudopasswd(self, arg, psswrd):
@@ -141,15 +118,57 @@ class askPassword():
         p = Popen(['sudo', '-S'] + command, stdin=PIPE, stderr=PIPE, universal_newlines=True)
         sudo_prompt = p.communicate(psswrd + '\n')[1]
         if "incorrect password" in sudo_prompt:
-            print('incorrect password')
             self.passwordentry.set_text("")
-            self.window.show_all()
+            if self.trytimes != 3:
+                self.window.show_all()
+            elif self.trytimes == 3:
+                Gtk.main_quit()
         elif "is not in the sudoers" in sudo_prompt:
-            print('user not in the sudoers')
-            Gtk.main_quit()
+            userNotsetup()
         else:
-            print('it work')
             Gtk.main_quit()
+
+class userNotsetup():
+    def delete(self, widget):
+        Gtk.main_quit()
+        Gtk.main_quit()
+
+    def __init__(self):
+        self.trytimes = 0
+        self.window = Gtk.Window()
+        #self.window.set_size_request(700, 500)
+        #self.window.set_resizable(False)
+        self.window.connect("delete_event", self.delete)
+        self.window.set_title("Sudo not setup")
+        self.window.set_border_width(0)
+        #self.window.set_icon_from_file("/usr/local/lib/gbi/logo.png")
+        box1 = Gtk.VBox(False, 0)
+        self.window.add(box1)
+        box1.show()
+        box2 = Gtk.VBox(False, 10)
+        box2.set_border_width(10)
+        box1.pack_start(box2, True, True, 0)
+        box2.show()
+        # Title
+        Title = Gtk.Label("<b><span size='large'>User not setup to use Sudo</span></b>")
+        Title.set_use_markup(True)
+        box2.pack_start(Title, False, False, 0)
+        box2 = Gtk.HBox(False, 10)
+        box2.set_border_width(5)
+        box1.pack_start(box2, False, False, 0)
+        box2.show()
+        self.table = Gtk.Table(1, 6, True)
+        self.button1 = Gtk.Button(label='Ok')
+        self.button1.connect("clicked", self.delete)
+        self.table.attach(self.button1, 5, 6, 0, 1)
+        self.button1.show()
+        self.table.set_col_spacings(5)
+        self.table.show()
+        # Add button
+        box2.pack_start(self.table,True, True, 5)
+        self.window.show_all()
+        Gtk.main()
+
 
 
 if len(sys.argv) == 2:
